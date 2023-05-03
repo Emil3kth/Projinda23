fa5c8cbb80a2f3297225d2bfe56e7ea790f40da0
Emil3kth <emil3@kth.se>
2023-05-03 17:26:35 +0200

    Bugfixes + grid based movement

--- Projinda Diablolite/Player/player.gd
+++ Projinda Diablolite/Player/player.gd
@@ -1,28 +1,36 @@
-extends CharacterBody2D
+extends Area2D
 
-@export var speed: int = 35
-@onready var animations = $AnimationPlayer
+var speed = 2
+var moving = false
+var tile_size = 16
+var inputs = {
+	"ui_right": Vector2.RIGHT,
+	"ui_left": Vector2.LEFT,
+	"ui_up": Vector2.UP,
+	"ui_down": Vector2.DOWN
+}
 
-func handleInput():	
-	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = moveDirection * speed
-	
-func updateAnimation():
-	if velocity.length() == 0:
-		if animations.is_playing():
-			animations.stop()
-	else:
-		var direction = "Down"
-		if velocity.x < 0: direction = "Left"
-		elif velocity.x > 0: direction = "Left_2"
-		elif velocity.y < 0: direction= "Up"
-	
-		animations.play("walk" + direction)
-	
-	
+@onready var ray = $RayCast2D
 
-func _physics_process(delta):
-	handleInput()
-	move_and_slide()
-	updateAnimation()
-
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size / 2
+	
+func _unhandled_input(event):
+	if moving:
+		return
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+			
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		#position += inputs[dir] * tile_size
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		$AnimationPlayer.play(dir)
+		await tween.finished
+		moving = false
